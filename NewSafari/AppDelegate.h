868620b4b30365fd4df159c49ff0d2c4a36ebb13
Albert Saucedo <saucedoa7@gmail.com>
2014-09-23 17:51:10 -0500

    Initial Commit

---
+++ NewSafari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NewSafari
+//
+//  Created by Albert Saucedo on 9/23/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
