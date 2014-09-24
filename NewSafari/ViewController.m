0274a9d531fbbe6835a5a918ed68ee0c49421296
Albert Saucedo <saucedoa7@gmail.com>
2014-09-23 19:54:35 -0500

    Finished MVP

--- NewSafari/ViewController.m
+++ NewSafari/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextField *URLTextField;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
 @end
 
 @implementation ViewController
@@ -17,13 +20,48 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.URLTextField.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)webViewDidFinishLoad:(UIWebView *)webView{
+
+    if (self.webView.canGoBack) {
+        self.backButton.enabled = YES;
+    } else {
+        self.backButton.enabled = NO;
+    }
+
+    if (self.webView.canGoForward) {
+        self.forwardButton.enabled = YES;
+    } else {
+        self.forwardButton.enabled = NO;
+    }
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [self.URLTextField resignFirstResponder];
+
+    NSString *urlString = [[NSString alloc] initWithFormat:@"http://%@.com", self.URLTextField.text ];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    NSLog(@"%@", request);
+
+    [self.webView loadRequest:request];
+    [self.view addSubview:self.webView];
+
+    return YES;
+}
+- (IBAction)onBackButtonPressed:(UIButton *)sender {
+    [self.webView goBack];
+}
+- (IBAction)onForwardButtonPressed:(UIButton *)sender {
+    [self.webView goForward];
+}
+- (IBAction)onStopButtonPressed:(UIButton *)sender {
+    [self.webView stopLoading];
+}
+- (IBAction)onReloadButtonPressed:(UIButton *)sender {
+    [self.webView reload];
 }
 
 @end
